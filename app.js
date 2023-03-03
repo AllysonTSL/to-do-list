d0cc1590b104d5217ab53cc9441a1d660896589e
AllysonTSL <92544632+AllysonTSL@users.noreply.github.com>
2023-03-03 17:18:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,87 @@
-const newTask       = document.querySelector("#new-task")
-const btnAddTask    = document.querySelector("#add-task")
-const todoList      = document.querySelector(".todo-list ul")
-
-document.querySelector(".complete-list ul")
+const newTask = document.querySelector("#new-task");
+const btnAddTask = document.querySelector("#add-task");
+const todoList = document.querySelector(".todo-list ul");
+const doneList = document.querySelector(".complete-list ul");
 
 // Funções
 const checkDone = function (event) {
-    const task = event.target.parentNode
-    const taskText = task.innerText
+  const task = event.target.parentNode;
+  const taskText = task.querySelector("span").innerText;
 
-    todoList.removeChild(task)
-}
+  todoList.removeChild(task);
+
+  const taskDoneExample = doneList.querySelector("#task-done-example");
+  if (taskDoneExample !== null) {
+    doneList.removeChild(taskDoneExample);
+  }
+
+  const undo = addListItem(doneList, taskText, true);
+
+  undo.addEventListener("change", function (ev) {
+    const taskUndo = undo.parentNode;
+    const taskUndoText = taskUndo.querySelector("span").innerText;
+
+    doneList.removeChild(taskUndo);
+
+    const cb = addListItem(todoList, taskUndoText);
+    cb.addEventListener("change", checkDone);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Deletar";
+    deleteButton.classList.add("delete");
+    taskUndo.appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", function (ev) {
+      doneList.removeChild(taskUndo);
+    });
+  });
+};
+
+const addListItem = function (list, value, checked = false) {
+  const li = document.createElement("li");
+  const check = document.createElement("input");
+  const label = document.createElement("label");
+  const deleteButton = document.createElement("button");
+  check.setAttribute("type", "checkbox");
+  li.appendChild(check);
+  label.innerHTML = <span>${value}</span>;
+  li.appendChild(label);
+  deleteButton.innerText = "Deletar";
+  deleteButton.classList.add("delete");
+  li.appendChild(deleteButton);
+  list.appendChild(li);
+
+  if (checked) {
+    check.setAttribute("checked", "checked");
+    label.querySelector("span").classList.add("done");
+  }
+
+  deleteButton.addEventListener("click", function (ev) {
+    const taskToRemove = deleteButton.parentNode;
+    list.removeChild(taskToRemove);
+  });
+
+  return check;
+};
+
 
 // Eventos
-btnAddTask.onclick = function() {
-    const taskExample = document.querySelector("#task-example")
-    if(taskExample !== null) {
-        todoList.removeChild(taskExample)
-        const li = document.createElement("lixo")
-        li.innerText = taskText
-        completeList.appendChild(lixo)
-    }
-    const li    = document.createElement("li")
-    const check = document.createElement("input") 
-    const label = document.createElement("label")
-    label.innerText = newTask.value
-    check.setAttribute("type", "checkbox")
-    li.appendChild(check)
-    li.appendChild(label)
-    todoList.appendChild(li)
+btnAddTask.onclick = function () {
+  const taskExample = document.querySelector("#task-example");
+  if (taskExample !== null) {
+    todoList.removeChild(taskExample);
+  }
 
-    newTask.value = ""
-    newTask.focus()
+  const check = addListItem(todoList, newTask.value);
 
-    check.addEventListener("change", checkDone)
+  newTask.value = "";
+  newTask.focus();
 
-}
+  check.addEventListener("change", checkDone);
+};
 
-newTask.onkeyup = function(event) {
-    if(event.keyCode === 13) {
-        btnAddTask.click()
-    }
-}
+newTask.onkeyup = function (event) {
+  if (event.keyCode === 13) {
+    btnAddTask.click();
+  }
+};
